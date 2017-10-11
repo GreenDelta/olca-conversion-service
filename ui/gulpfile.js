2ee8162fdce5c980a85bde25069dbb5a13e8015a
msrocka <michael.srocka@gmail.com>
2017-10-11 09:54:08 +0200

    ui setup

---
+++ ui/gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require("gulp");
+const clean = require('gulp-clean');
+const merge = require('merge-stream');
+const sourcemaps = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
+
+gulp.task('default', ['copy'], () => {
+    return gulp.src(['src/**/*.ts', 'src/**/*.tsx'])
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            jsx: 'react',
+            module: 'amd',
+            moduleResolution: 'node',
+            out: 'app.js',
+            sourceMap: true,
+            removeComments: true,
+            target: 'es5'
+        })).js.pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('copy', ['clean'], () => {
+    const html = gulp.src(['src/html/*.html', 'src/html/*.css'])
+        .pipe(gulp.dest('build'));
+    const libs = gulp.src([
+        'node_modules/requirejs/require.js',
+        'node_modules/react/dist/react.min.js',
+        'node_modules/react-dom/dist/react-dom.min.js'
+    ]).pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('clean', () => {
+    return gulp.src(['build'], {
+        read: false
+    }).pipe(clean({ force: true }));
+});
